267b9c7c3c856fc081450cc8f796dd38ce664f39
beardyman <nornholdj@gmail.com>
2016-02-23 00:31:15 -0500

    added new exception type and unwrapped example for webhooks

---
+++ examples/unwrapped/get_webhooks.php
@@ -0,0 +1,25 @@
+<?php
+namespace Examples\Unwrapped;
+require_once (dirname(__FILE__).'/../bootstrap.php');
+
+//pull in API key config
+$configFile = file_get_contents(dirname(__FILE__) . '/../example-config.json');
+$config = json_decode($configFile, true);
+
+use SparkPost\SparkPost;
+use GuzzleHttp\Client;
+use Ivory\HttpAdapter\Guzzle6HttpAdapter;
+
+$httpAdapter = new Guzzle6HttpAdapter(new Client());
+$sparky = new SparkPost($httpAdapter, ['key'=>$config['api-key']]);
+
+try {
+  $sparky->setupUnwrapped('webhooks');
+
+  $results = $sparky->webhooks->get();
+
+  echo 'Congrats you can use your SDK!';
+} catch (\Exception $exception) {
+  echo $exception->getMessage();
+}
+?>
