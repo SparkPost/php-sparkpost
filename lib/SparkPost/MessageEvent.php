57c509fd9110dc33f5704020bdb72b82796fb2d9
James Fellows <james.fellows@finanscapes.com>
2016-04-04 21:28:34 +0100

    Add tests for new MessageEvent DateTime handling

--- lib/SparkPost/MessageEvent.php
+++ lib/SparkPost/MessageEvent.php
@@ -2,8 +2,9 @@
 namespace SparkPost;
 
 /**
- * SDK class for querying message events API
- * @package SparkPost
+ * SDK class for querying the Message Events API
+ *
+ * @see https://developers.sparkpost.com/api/#/reference/message-events
  */
 class MessageEvent extends APIResource
 {
@@ -12,7 +13,7 @@ class MessageEvent extends APIResource
   /**
    * Method for issuing search requests to the Message Events API.
    *
-   * The method passes-through all of the query parameters listed at
+   * The method passes-through all of the query parameters - the valid ones are listed at
    * @link https://developers.sparkpost.com/api/#/reference/message-events/events-documentation/search-for-message-events
    *
    * @param array $queryParams  The query parameters.  Note that a query parameter containing an array
@@ -22,6 +23,30 @@ class MessageEvent extends APIResource
    */
   public function search(Array $queryParams)
   {
+    // check for DateTime objects & replace them with the formatted string equivalent
+    foreach(["from", "to"] as $dateTimeParam) {
+      if (isset($queryParams[$dateTimeParam]) && $queryParams[$dateTimeParam] instanceof \DateTime) {
+        // the message events API doesn't allow the seconds or GMT offset, so strip them
+        $queryParams[$dateTimeParam] = substr($queryParams[$dateTimeParam]->format(\DateTime::ATOM), 0, 16);
+      }
+    }
+
     return $this->get(null, $queryParams);
+  }
+
+  /**
+   * List descriptions of the event fields that could be included in a response from the MessageEvent::search() method.
+   *
+   * @return array The event field descriptions.
+   */
+  public function documentation() {
+    return $this->get("events/documentation");
+  }
+
+  /**
+   * List an example of the event data that will be included in a response from the MessageEvent::search() method.
+   */
+  public function samples() {
+    return $this->get("events/samples", ["events" => "bounce"]);
   }
 }
---
+++ test/unit/MessageEventTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace SparkPost;
+
+use \Mockery;
+
+
+class MessageEventTest extends \PHPUnit_Framework_TestCase
+{
+  private $sparkPostMock;
+  private $sut;
+
+  /**
+   * (non-PHPdoc)
+   * @before
+   * @see PHPUnit_Framework_TestCase::setUp()
+   */
+  public function setUp()
+  {
+    $this->sparkPostMock = Mockery::mock('SparkPost\SparkPost', function ($mock) {
+      $mock->shouldReceive('getHttpHeaders')->andReturn([]);
+    });
+    $this->sparkPostMock->httpAdapter = Mockery::mock();
+    $this->sut = new MessageEvent($this->sparkPostMock);
+  }
+
+  public function testDateTimeConversion()
+  {
+    $testBody = ['results' => ['my' => 'test']];
+    $testFrom = new \DateTime("1978-08-27 04:05:02");
+    $testFromStr = urlencode("1978-08-27T04:05");
+    $testTo = new \DateTime("2016-04-04 19:00");
+    $testToStr = urlencode("2016-04-04T19:00");
+
+    $responseMock = Mockery::mock();
+    $this->sparkPostMock->httpAdapter->shouldReceive('send')->
+      once()->
+      with("/message-events/?from={$testFromStr}&to={$testToStr}", 'GET', Mockery::type('array'), null)->
+      andReturn($responseMock);
+    $responseMock->shouldReceive('getStatusCode')->andReturn(200);
+    $responseMock->shouldReceive('getBody->getContents')->andReturn(json_encode($testBody));
+
+    $this->assertEquals($testBody, $this->sut->search(["from" => $testFrom, "to" => $testTo]));
+  }
+}
