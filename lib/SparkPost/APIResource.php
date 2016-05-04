dc698dcebc7b35616ba8cff64527bdde0b39812a
Rich Leland <rich.leland@messagesystems.com>
2016-05-03 22:02:30 -0400

    Missed a merge marker or two

--- lib/SparkPost/APIResource.php
+++ lib/SparkPost/APIResource.php
@@ -47,9 +47,9 @@ class APIResource
     /**
      * Private Method helper to reference parameter mappings and set the right value for the right parameter.
      *
-     * @param array $model (pass by reference) the set of values to map
+     * @param array  $model  (pass by reference) the set of values to map
      * @param string $mapKey a dot syntax path determining which value to set
-     * @param mixed $value value for the given path
+     * @param mixed  $value  value for the given path
      */
     protected function setMappedValue(&$model, $mapKey, $value)
     {
@@ -80,7 +80,7 @@ class APIResource
      * maps values from the passed in model to those needed for the request.
      *
      * @param array $requestConfig the passed in model
-     * @param array $model the set of defaults
+     * @param array $model         the set of defaults
      *
      * @return array A model ready for the body of a request
      */
@@ -124,7 +124,7 @@ class APIResource
      * Wrapper method for issuing GET request to current API endpoint.
      *
      * @param string $resourcePath (optional) string resource path of specific resource
-     * @param array $query (optional) query string parameters
+     * @param array  $query        (optional) query string parameters
      *
      * @return array Result of the request
      */
@@ -137,7 +137,7 @@ class APIResource
      * Wrapper method for issuing DELETE request to current API endpoint.
      *
      * @param string $resourcePath (optional) string resource path of specific resource
-     * @param array $query (optional) query string parameters
+     * @param array  $query        (optional) query string parameters
      *
      * @return array Result of the request
      */
@@ -150,130 +150,116 @@ class APIResource
      * assembles a URL for a request.
      *
      * @param string $resourcePath path after the initial endpoint
-     <<<<<<< HEAD
-   * @param array $options array with an optional value of query with values to build a querystring from.
-   *
-=======
-   * @param array $options array with an optional value of query with values to build a querystring from.  Any
-   *                        query elements that are themselves arrays will be imploded into a comma separated list.
->>>>>>> master
-   * @return string the assembled URL
-   */
-  private function buildUrl($resourcePath, $options)
-  {
-      $url = "/{$this->endpoint}/";
-      if (!is_null($resourcePath)) {
-          $url .= $resourcePath;
-      }
-
-<<<<<<< HEAD
-      if (!empty($options['query'])) {
-          $queryString = http_build_query($options['query']);
-          $url .= '?'.$queryString;
-      }
-=======
-    if( !empty($options['query'])) {
-      // check each query element - if it's an array, implode it to match the API-accepted format
-      foreach($options['query'] as &$element) {
-        if(is_array($element)) {
-          $element = implode(",", $element);
-        }
-      }
-
-      $queryString = http_build_query($options['query']);
-      $url .= '?'.$queryString;
-    }
->>>>>>> master
-
-      return $url;
-  }
-
-  /**
-   * Prepares a body for put and post requests.
-   *
-   * @param array $options array with an optional value of body with values to build a request body from.
-   *
-   * @return string|null A json encoded string or null if no body was provided
-   */
-  private function buildBody($options)
-  {
-      $body = null;
-      if (!empty($options['body'])) {
-          $model = static::$structure;
-          $requestModel = $this->buildRequestModel($options['body'], $model);
-          $body = json_encode($requestModel);
-      }
-
-      return $body;
-  }
-
-  /**
-   * Private Method for issuing GET and DELETE request to current API endpoint.
-   *
-   *  This method is responsible for getting the collection _and_
-   *  a specific entity from the API endpoint
-   *
-   *  If resourcePath parameter is omitted, then we fetch the collection
-   *
-   * @param string $action HTTP method type
-   * @param string $resourcePath (optional) string resource path of specific resource
-   * @param array $options (optional) query string parameters
-   *
-   * @return array Result set of action performed on resource
-   *
-   * @throws APIResponseException
-   */
-  private function callResource($action, $resourcePath = null, $options = [])
-  {
-      $action = strtoupper($action); // normalize
-
-    $url = $this->buildUrl($resourcePath, $options);
-      $body = $this->buildBody($options);
-
-    //make request
-    try {
-        $response = $this->sparkpost->httpAdapter->send($url, $action, $this->sparkpost->getHttpHeaders(), $body);
-
-        $statusCode = $response->getStatusCode();
-
-      // Handle 4XX responses, 5XX responses will throw an HttpAdapterException
-      if ($statusCode < 400) {
-        return json_decode($response->getBody()->getContents(), true);
-      }
-      elseif ($statusCode === 403) {
-        $response = json_decode($response->getBody(), true);
-        throw new APIResponseException(
-          'Request forbidden',
-          $statusCode,
-          isset($response['errors'][0]['message']) ? $response['errors'][0]['message'] : "Request forbidden",
-          isset($response['errors'][0]['code']) ? $response['errors'][0]['code'] : 1100,
-          isset($response['errors'][0]['description']) ? $response['errors'][0]['description'] : "Does this API Key have the correct permissions?"
-        );
-      }
-      elseif ($statusCode === 404) {
-        throw new APIResponseException('The specified resource does not exist', 404);
-      }
-      else {
-        $response = json_decode($response->getBody(), true);
-        throw new APIResponseException(
-          'Received bad response from ' . ucfirst($this->endpoint),
-          $statusCode,
-          isset($response['errors'][0]['message']) ? $response['errors'][0]['message'] : "",
-          isset($response['errors'][0]['code']) ? $response['errors'][0]['code'] : 0,
-          isset($response['errors'][0]['description']) ? $response['errors'][0]['description'] : ""
-        );
-      }
-    }
-
-    /*
-     * Configuration Errors, and a catch all for other errors
+     * @param array  $options      array with an optional value of query with values to build a querystring from.  Any
+     *                             query elements that are themselves arrays will be imploded into a comma separated list.
+     *
+     * @return string the assembled URL
      */
-    catch (\Exception $exception) {
-        if ($exception instanceof APIResponseException) {
-            throw $exception;
+    private function buildUrl($resourcePath, $options)
+    {
+        $url = "/{$this->endpoint}/";
+        if (!is_null($resourcePath)) {
+            $url .= $resourcePath;
         }
 
-        throw new APIResponseException('Unable to contact ' . ucfirst($this->endpoint) . ' API: ' . $exception->getMessage(), $exception->getCode());
+        if (!empty($options['query'])) {
+            // check each query element - if it's an array, implode it to match the API-accepted format
+            foreach ($options['query'] as &$element) {
+                if (is_array($element)) {
+                    $element = implode(',', $element);
+                }
+            }
+
+            $queryString = http_build_query($options['query']);
+            $url .= '?'.$queryString;
+        }
+
+        return $url;
     }
-  }
+
+    /**
+     * Prepares a body for put and post requests.
+     *
+     * @param array $options array with an optional value of body with values to build a request body from.
+     *
+     * @return string|null A json encoded string or null if no body was provided
+     */
+    private function buildBody($options)
+    {
+        $body = null;
+        if (!empty($options['body'])) {
+            $model = static::$structure;
+            $requestModel = $this->buildRequestModel($options['body'], $model);
+            $body = json_encode($requestModel);
+        }
+
+        return $body;
+    }
+
+    /**
+     * Private Method for issuing GET and DELETE request to current API endpoint.
+     *
+     *  This method is responsible for getting the collection _and_
+     *  a specific entity from the API endpoint
+     *
+     *  If resourcePath parameter is omitted, then we fetch the collection
+     *
+     * @param string $action       HTTP method type
+     * @param string $resourcePath (optional) string resource path of specific resource
+     * @param array  $options      (optional) query string parameters
+     *
+     * @return array Result set of action performed on resource
+     *
+     * @throws APIResponseException
+     */
+    private function callResource($action, $resourcePath = null, $options = [])
+    {
+        $action = strtoupper($action); // normalize
+
+        $url = $this->buildUrl($resourcePath, $options);
+        $body = $this->buildBody($options);
+
+        //make request
+        try {
+            $response = $this->sparkpost->httpAdapter->send($url, $action, $this->sparkpost->getHttpHeaders(), $body);
+
+            $statusCode = $response->getStatusCode();
+
+            // Handle 4XX responses, 5XX responses will throw an HttpAdapterException
+            if ($statusCode < 400) {
+                return json_decode($response->getBody()->getContents(), true);
+            } elseif ($statusCode === 403) {
+                $response = json_decode($response->getBody(), true);
+                throw new APIResponseException(
+                    'Request forbidden',
+                    $statusCode,
+                    isset($response['errors'][0]['message']) ? $response['errors'][0]['message'] : 'Request forbidden',
+                    isset($response['errors'][0]['code']) ? $response['errors'][0]['code'] : 1100,
+                    isset($response['errors'][0]['description']) ? $response['errors'][0]['description'] : 'Does this API Key have the correct permissions?'
+                );
+            } elseif ($statusCode === 404) {
+                throw new APIResponseException('The specified resource does not exist', 404);
+            } else {
+                $response = json_decode($response->getBody(), true);
+                throw new APIResponseException(
+                    'Received bad response from '.ucfirst($this->endpoint),
+                    $statusCode,
+                    isset($response['errors'][0]['message']) ? $response['errors'][0]['message'] : '',
+                    isset($response['errors'][0]['code']) ? $response['errors'][0]['code'] : 0,
+                    isset($response['errors'][0]['description']) ? $response['errors'][0]['description'] : ''
+                );
+            }
+        }
+
+        /*
+         * Configuration Errors, and a catch all for other errors
+         */
+        catch (\Exception $exception) {
+            if ($exception instanceof APIResponseException) {
+                throw $exception;
+            }
+
+            throw new APIResponseException('Unable to contact '.ucfirst($this->endpoint).' API: '.$exception->getMessage(), $exception->getCode());
+        }
+    }
 }
