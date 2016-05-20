af5169612d4fe02824383cd8e8fdbe0a108ff246
Rich Leland <rich.leland@messagesystems.com>
2016-05-20 09:26:30 -0400

    Add example for unwrapped transmissions call

---
+++ examples/unwrapped/create_transmission.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Examples\Unwrapped;
+
+require_once dirname(__FILE__).'/../bootstrap.php';
+
+//pull in API key config
+$configFile = file_get_contents(dirname(__FILE__).'/../example-config.json');
+$config = json_decode($configFile, true);
+
+use SparkPost\SparkPost;
+use GuzzleHttp\Client;
+use Ivory\HttpAdapter\Guzzle6HttpAdapter;
+
+$httpAdapter = new Guzzle6HttpAdapter(new Client());
+$sparky = new SparkPost($httpAdapter, ['key' => $config['api-key']]);
+
+try {
+    // define the endpoint
+    $sparky->setupUnwrapped('transmissions');
+
+    $message = [
+        'recipients' => [
+            [
+                'address' => [
+                    'email' => 'john.doe@example.com',
+                ]
+            ]
+        ],
+        'content' => [
+            'from' => [
+                'name' => 'From Envelope',
+                'email' => 'from@sparkpostbox.com',
+            ],
+            'html' => '<p>Hello World!</p>',
+            'text' => 'Hello World!',
+            'subject' => 'Example Email'
+        ]
+    ];
+    $results = $sparky->transmissions->create($message);
+    echo 'Congrats! You sent a message using SparkPost!';
+} catch (\Exception $exception) {
+    echo $exception->getAPIMessage()."\n";
+    echo $exception->getAPICode()."\n";
+    echo $exception->getAPIDescription()."\n";
+}
+
