c0e8628835038868b4a7caef93a1ab07c43d70fc
Avi Goldman <avrahamymgoldman@gmail.com>
2016-06-14 10:31:12 -0400

    added comments in SparkPostPromise

--- lib/SparkPost/SparkPostPromise.php
+++ lib/SparkPost/SparkPostPromise.php
@@ -6,12 +6,28 @@ use Http\Promise\Promise as HttpPromise;
 
 class SparkPostPromise implements HttpPromise
 {
+
+	/**
+	 * HttpPromise to be wrapped by SparkPostPromise
+	 */ 
 	private $promise;
 
+	/**
+	 * set the promise to be wrapped
+	 *
+	 * @param HttpPromise $promise
+	 */
 	public function __construct(HttpPromise $promise) {
 		$this->promise = $promise;
 	}
 
+
+	/**
+	 * Hand off the response functions to the original promise and return a custom response or exception
+	 *
+	 * @param callable $onFulfilled - function to be called if the promise is fulfilled
+	 * @param callable $onRejected - function to be called if the promise is rejected
+	 */ 
 	public function then(callable $onFulfilled = null, callable $onRejected = null) {
 		$this->promise->then(
 		function($response) {
@@ -22,11 +38,23 @@ class SparkPostPromise implements HttpPromise
 		});
 	}
 
+	/**
+	 * Hand back the state
+	 *
+	 * @return $state - returns the state of the promise
+	 */
 	public function getState() {
 		return $this->promise->getState();
 	}
 
-
+	/**
+	 * Wraps the wait function and returns a custom response or throws a custom exception
+	 *
+	 * @param boolean $unwrap
+	 *
+	 * @return SparkPostResponse
+	 * @throws SparkPostException
+	 */ 
 	public function wait($unwrap = true) {
 		try
 		{
