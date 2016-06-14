9ad05b59d0c2a6534393d910ce162221713e1154
Avi Goldman <avrahamymgoldman@gmail.com>
2016-06-14 10:33:43 -0400

    added comments in SparkPostException

--- lib/SparkPost/SparkPostException.php
+++ lib/SparkPost/SparkPostException.php
@@ -6,8 +6,16 @@ use Http\Client\Exception\HttpException as HttpException;
 
 class SparkPostException extends \Exception {
 
+    /**
+     * Variable to hold json decoded body from http response
+     */
     private $body = null;
 
+    /**
+     * Sets up the custom exception and copies over original exception values
+     *
+     * @param Exception $exception - the exception to be wrapped
+     */
     public function __construct(\Exception $exception) {
         $message = $exception->getMessage();
         if($exception instanceof HttpException) {
@@ -18,6 +26,12 @@ class SparkPostException extends \Exception {
         parent::__construct($message, $exception->getCode(), $exception->getPrevious());
     }
 
+
+    /**
+     * Returns the body
+     *
+     * @return array $body - the json decoded body from the http response
+     */
     public function getBody() {
         return $this->body;
     }
