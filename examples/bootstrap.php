6bcfbd1a29015b318e86c435a11cab9607560125
Avi Goldman <avrahamymgoldman@gmail.com>
2017-01-09 13:09:59 -0500

    removed example-options.json

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -1,7 +1,3 @@
 <?php
 
 require_once dirname(__FILE__).'/../vendor/autoload.php';
-
-//pull in library options
-$optionsFile = file_get_contents(dirname(__FILE__).'/example-options.json');
-$options = json_decode($optionsFile, true);
--- post-install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-if [ ! -f 'examples/example-options.json' ]; then echo '{\n\t"key":"YOUR_API_KEY"\n}' >> examples/example-options.json; fi
